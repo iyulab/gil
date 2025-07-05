61933dbbef1b356e08d3033a3cfe159df403e813
uj <caveman@iyulab.com>
2025-07-05 16:17:38 +0900

    site-publish

--- gil-flow-site/next.config.ts
+++ gil-flow-site/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'publish',
+  distDir: '../site-publish',
   /* config options here */
 };
 
